4ddf9848eca5916c95890af59f48f17aaedc02f3
Misha Grinko <glukym@gmail.com>
2024-03-13 10:43:50 +0200

    use custom API_URL

--- src/api/fetchClient.ts
+++ src/api/fetchClient.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const client = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
